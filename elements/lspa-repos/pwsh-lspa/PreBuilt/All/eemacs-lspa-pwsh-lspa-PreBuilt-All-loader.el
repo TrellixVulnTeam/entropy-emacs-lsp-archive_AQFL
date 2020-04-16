95046f895c069827169bda18b40ff20dbc427633
Entropy <bmsac0001@gmail.com>
2020-04-16 11:44:14 +0800

    Fix bug of pwsh-lspa specification

--- elements/lspa-repos/pwsh-lspa/PreBuilt/All/eemacs-lspa-pwsh-lspa-PreBuilt-All-loader.el
+++ elements/lspa-repos/pwsh-lspa/PreBuilt/All/eemacs-lspa-pwsh-lspa-PreBuilt-All-loader.el
@@ -17,7 +17,12 @@
  :load-body
  ((progn
     (setq lsp-pwsh-dir
-          eemacs-lspa/pwsh-lspa-PreBuilt-All-loader--pwsh-lspa-dir))))
+          eemacs-lspa/pwsh-lspa-PreBuilt-All-loader--pwsh-lspa-dir)
+    (with-eval-after-load 'lsp-pwsh
+      ;; ensure that the log path exist or will make pwsh-ls start fail
+      (let ()
+        (unless (file-exists-p lsp-pwsh-log-path)
+          (mkdir lsp-pwsh-log-path 'create-parent))))))
 
 ;; * Provide
 (provide 'eemacs-lspa-pwsh-lspa-PreBuilt-All-loader)
