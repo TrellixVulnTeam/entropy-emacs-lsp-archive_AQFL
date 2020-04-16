84e8e10946808dcfad49a81ee57973f3f6e010be
Entropy <bmsac0001@gmail.com>
2020-04-16 08:41:48 +0800

    Add gopls language server

---
+++ elements/lspa-repos/go-lspa/PreBuilt/GnuLinux/x86_64/eemacs-lspa-go-lspa-PreBuilt-GnuLinux-x86_64-loader.el
@@ -0,0 +1,24 @@
+;; * Code
+;; ** Require
+(require 'eemacs-lspa-subr)
+;; ** Variable Declaration
+(defvar eemacs-lspa/go-lspa-PreBuilt-GnuLinux-x86_64-loader--root (file-name-directory (expand-file-name load-file-name)))
+(defvar eemacs-lspa/go-lspa-PreBuilt-GnuLinux-x86_64-loader--gopls-dir
+  (expand-file-name
+   "gopls"
+   eemacs-lspa/go-lspa-PreBuilt-GnuLinux-x86_64-loader--root))
+
+;; ** Main
+;; Use `eemacs-lspa/subr-common-do-with-prompt' for export loader
+
+(eemacs-lspa/subr-common-do-with-prompt
+ "Init gopls ..."
+ "Add gopls to path ..."
+ :load-body
+ ((progn
+    (eemacs-lspa/subr-add-path
+     eemacs-lspa/go-lspa-PreBuilt-GnuLinux-x86_64-loader--gopls-dir nil
+     eemacs-lspa/go-lspa-PreBuilt-GnuLinux-x86_64-loader--gopls-dir nil))))
+
+;; * Provide
+(provide 'eemacs-lspa-go-lspa-PreBuilt-GnuLinux-x86_64-loader)
